16d66a4856b4fdc89061fd49916d5220e492f977
afgbloch <bloch.aurelien@gmail.com>
2018-07-02 15:28:01 +0200

    update build luajit script for auditlog

--- tests/travis/build_luajit.sh
+++ tests/travis/build_luajit.sh
@@ -4,7 +4,7 @@ set -ex
 cd lib/luajit-rj
 git checkout auditlog
 git pull
-make amalg PREFIX=`pwd`
+make -j
 make install PREFIX=`pwd`
 mv bin/luajit{-2.1.0-beta3,}
 cp src/libluajit.a ../../bin/linux
